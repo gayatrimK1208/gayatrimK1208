20522f9a37b7fbfa7cab723a0250608cea99cc1f
gayatrimK1208 <GAYATRIMK1208@GMAIL.COM>
2025-01-23 07:20:08 -0500

    Update S2Exercise1.c

--- S2Exercise1.c
+++ S2Exercise1.c
@@ -5,6 +5,8 @@
 #define BUFSIZE 256    
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
+pid_t parent = getpid();
+pid_t pid = fork();
 
     // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
@@ -13,13 +15,32 @@ int main(int argc, char** argv) {
     }
     char cmd[BUFSIZE] = "wc -c < ";
 // Usage of the strncat function allows to specify the maximum number of characters to append, ensuring you stay within bounds.
- if (argv[1] != NULL) {
+ if (argv[1] != NUL) {
     // Append argv[1] safely
     strncat(cmd, argv[1], BUFSIZE - strlen(cmd) - 1);
  } else {
     fprintf(stderr, "Error: Missing argument\n");
     return 1;
 }   
-      system(cmd); 
+
+ pid_t parent = getpid();
+pid_t pid = fork();
+
+if (pid == -1)
+{
+     fprintf(stderr, "Error: Fork not available");
+} 
+else if (pid > 0)
+{
+    int status;
+    waitpid(pid, &status, 0);
+}
+else 
+{
+    // we are the child
+    execve(cmd);
+    _exit(EXIT_FAILURE);   // exec never returns
+}
+     // system(cmd); 
     
 }
