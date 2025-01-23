6bb32b6b03eefeb5aedf1456eab60d7e698415e4
gayatrimK1208 <GAYATRIMK1208@GMAIL.COM>
2025-01-23 06:27:53 -0500

    Update S2Exercise1.c

--- S2Exercise1.c
+++ S2Exercise1.c
@@ -5,8 +5,7 @@
 #define BUFSIZE 256    
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
- int maxLength=getconf ARG_MAX;
- 
+
     // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
         fprintf(stderr, "Please provide the address of a file as an input.\n");
